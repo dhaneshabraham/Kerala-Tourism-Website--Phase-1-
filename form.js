af216ae2b5cceef70c9729a19ccd5e4d7949dd07
dhaneshabraham <dhaneshabraham@gmail.com>
2022-03-18 12:39:11 +0530

    login updates

---
+++ form.js
@@ -0,0 +1,54 @@
+function loginvalidation(){
+    var x=document.getElementsByClassName("input")
+    var y=document.getElementsByTagName("label");
+    if(x[0].value=="" ||x[1].value=="") 
+    {
+        y[2].style.color="red"
+        y[3].style.color="red"
+        alert("cannot left empty")
+        
+        return false
+    }       
+    else
+    {            
+    return true
+    }
+    
+    }
+function signupvalidation(){
+    
+    var x=document.getElementsByClassName("input")
+    var y=document.getElementsByTagName("label");
+    var err=document.getElementsByClassName("laberr");
+    if(x[2].value=="" ||x[3].value==""||x[4].value=="" ||x[5].value=="") 
+    {
+        y[5].style.color="red"
+        y[6].style.color="red"
+        y[7].style.color="red"
+        y[8].style.color="red"
+        alert("cannot left empty")
+        return false
+    }       
+    else
+    {
+        let emailRegexp=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        let userRegexp=/^[a-zA-Z0-9\.-]$/;
+        var em=emailRegexp.test(x[2].value);
+        var us=userRegexp.test(x[3].value);
+        if (!em||!us)
+        {              
+            err[0].innerHTML="Invalid email id or username";
+            return false;
+        }
+        else
+        {
+            if(x[4].value!=x[5].value)
+            {
+                err[0].innerHTML="password doesnot match !";
+                return false;
+            }
+        }
+        
+        return true
+    }
+    }
